9ab70e30c721d7623ed7689af34063ca6f89a5cd
Michael Gerischer <mi.gerischer@gmail.com>
2022-01-28 15:32:44 +0100

    enabled using grunt file for multiple files

--- Next Birthdays/Gruntfile.js
+++ Next Birthdays/Gruntfile.js
@@ -4,15 +4,17 @@ module.exports = function(grunt) {
 
         pkg: grunt.file.readJSON('package.json'),
 
-        clean: [`dist/`],
+        clean: {
+            nb_dist: [`dist/`]
+        },
 
         concat: {
-            core: {
+            nb_core: {
                 src: ["src/core/NextBirthdays_Header.js", "src/core/modules/PersistenceService.js", "src/core/modules/Initializer.js", 
                 "src/core/modules/PropertyHandler.js", "src/core/modules/Update.js", "src/core/NextBirthdays.js"],
                 dest: "dist/NextBirthdays.js"
             },
-            plugin: {
+            nb_plugin: {
 
             }
         }
@@ -22,6 +24,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-clean');
 
     // Next Birthdays
-    grunt.registerTask('default', 'Build Next Birthdays', ['clean', 'concat']);
+    grunt.registerTask('default', 'Build Next Birthdays', ['clean:nb_dist', 'concat:nb_core', 'concat:nb_plugin']);
   
   };
