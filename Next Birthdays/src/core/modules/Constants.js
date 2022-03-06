4c19b4055dbcc815944f2501285c8b44119d2b10
Michael Gerischer <mi.gerischer@gmail.com>
2022-03-06 18:00:17 +0100

    fixed github url

--- Next Birthdays/src/core/modules/Constants.js
+++ Next Birthdays/src/core/modules/Constants.js
@@ -1,7 +1,7 @@
 class Constants{
 
     static META_FILE_SUFFIX = ".meta.json";
-    static REPO_URL = "https://github.com/GerMichael/Scriptable_Files/tree/new_approach/";
+    static REPO_URL = "https://raw.githubusercontent.com/GerMichael/Scriptable_Files/new_approach/";
     static SCRIPT_NAME_ATTR = "script";
     static SCRIPT_ORIGIN_ATTR = "origin";
     static VERSION_ATTR = "version";
